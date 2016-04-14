84b3769a6505ff8ef26a689914367d60152da622
cjjujubee <connie.jew@gmail.com>
2016-04-14 14:50:36 -0700

    Adds question and answer reveal after all questions answered.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,38 @@
 // Question List
 var question1 = {
-	question: 'question1',
-	options: ['option1', 'option2', 'option3', 'option4'], 
-	correctAnswer: 0
+	question: 'What was the first national park created in the United States?',
+	options: ['Acadia', 'Grand Canyon', 'Yellowstone', 'Yosemite'], 
+	correctAnswer: 2
 }
 
 
 
 var question2 = {
-	question: 'question2',
-	options: ['question 2 option1', 'option2', 'option3', 'option4'],
-	correctAnswer: 0
+	question: 'Which national park has the greatest number of visitors each year?',
+	options: ['Yellowstone', 'Rocky Mountain', 'Zion', 'Great Smoky Mountains'],
+	correctAnswer: 3
 }
 
 
 
 
 var question3 = {
-	question: 'question3',
-	options: ['question 3 option1', 'option2', 'option3', 'option4'],
-	correctAnswer: 0
+	question: 'Which president signed an act that created the National Park Service?',
+	options: ['Theodore Roosevelt', 'Woodrow Wilson', 'Thomas Jefferson', 'Ulysses S. Grant'],
+	correctAnswer: 1
 }
 
 
 
 
 var question4 = {
-	question: 'question4',
-	options: ['option1', 'option2', 'option3', 'option4'], 
-	correctAnswer: 0
+	question: 'What is the largest national park?',
+	options: ['Death Valley', 'Glacier', 'Wrangell-St.Elias', 'Denali'], 
+	correctAnswer: 2
 }
 
 
-
-
-var question5 = {
-	question: 'question5',
-	options: ['option1', 'option2', 'option3', 'option4'], 
-	correctAnswer: 0
-}
-
-var listOfQuestions = [question1, question2, question3, question4, question5];
+var listOfQuestions = [question1, question2, question3, question4];
 
 var currentQuestion = 0;
 
@@ -87,10 +79,11 @@ $('#submitButton').click(function(event) {
 	// gets correct answers
 
 	var currentCorrectAnswer = listOfQuestions[currentQuestion].correctAnswer;
+	console.log(currentCorrectAnswer);
+	console.log(currentUserAnswer);
 
-	if (currentUserAnswer === currentCorrectAnswer) {
+	if (currentUserAnswer == currentCorrectAnswer) {
 		userScore++
-		console.log(userScore++);
 	}
 
 	$('.userScoreTally').text(userScore);
@@ -99,6 +92,25 @@ $('#submitButton').click(function(event) {
 	//increment currentQuestion
 
 	currentQuestion++;
+
+	if (currentQuestion >= listOfQuestions.length) {
+		console.log('current answers');
+		for(var i = 0; i < listOfQuestions.length; i++) {
+			var CQ = listOfQuestions[i]
+		var clonedQuestion = $('#clone').clone();
+		clonedQuestion.find('h4').text(CQ.question);
+		(function() {
+			for(var i = 0; i < CQ.options.length; i++) {
+				var option = CQ.options[i]
+				clonedQuestion.find('ol').append('<li>' + option +'</li>')
+			}
+		})();
+		$('.content').hide();
+
+		clonedQuestion.show();
+		$('#correctAnswers > ul').append(clonedQuestion);
+		}
+	}
 
 
 
