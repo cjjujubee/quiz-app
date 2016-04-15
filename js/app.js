21005024b3d63b95eddb8ce41bd4b741c634e61d
cjjujubee <connie.jew@gmail.com>
2016-04-14 23:05:40 -0700

    Adds ability to reset quiz and display correct answers at end of quiz

--- js/app.js
+++ js/app.js
@@ -1,79 +1,62 @@
+$(document).ready(function(){
+	updateDisplay();
+});
 // Question List
+
+
 var question1 = {
+	question: 'How many national parks are in the United States?',
+	options: ['100', '51', '58', '37'], 
+	correctAnswer: 2
+}
+
+var question2 = {
 	question: 'What was the first national park created in the United States?',
 	options: ['Acadia', 'Grand Canyon', 'Yellowstone', 'Yosemite'], 
 	correctAnswer: 2
 }
 
-
-
-var question2 = {
+var question3 = {
 	question: 'Which national park has the greatest number of visitors each year?',
 	options: ['Yellowstone', 'Rocky Mountain', 'Zion', 'Great Smoky Mountains'],
 	correctAnswer: 3
 }
 
-
-
-
-var question3 = {
+var question4 = {
 	question: 'Which president signed an act that created the National Park Service?',
 	options: ['Theodore Roosevelt', 'Woodrow Wilson', 'Thomas Jefferson', 'Ulysses S. Grant'],
 	correctAnswer: 1
 }
 
-
-
-
-var question4 = {
+var question5 = {
 	question: 'What is the largest national park?',
 	options: ['Death Valley', 'Glacier', 'Wrangell-St.Elias', 'Denali'], 
 	correctAnswer: 2
 }
 
 
-var listOfQuestions = [question1, question2, question3, question4];
+var listOfQuestions = [question1, question2, question3, question4, question5];
 
 var currentQuestion = 0;
 
 var userScore = 0;
+
+
+// Enables submit button
 
 $('p > input').change(function() {
 
 	$('#submitButton').prop('disabled', false).removeClass('disabled');
 });
 
-// Submit Button //
+// Submit Button 
 
 $('#submitButton').click(function(event) {
 	event.preventDefault();
 
-	//remove old question and answers
-
-
-	//go to next question
-
-
-	//replace question
-
-	$('#question').text(listOfQuestions[currentQuestion].question);
-
-
-	
 	//captures user selection
 	var currentUserAnswer = $("input[name=userGuess]:checked").val();
 
-	//replace list of answers
-
-	for(var i = 0; i < 4; i++ ) {
-
-		$('#choice' + (i + 1) ).text(listOfQuestions[currentQuestion].options[i]);
-
-	}
-
-	//resets radio button
-
-	$('input[name="userGuess"]').prop('checked', false);
 
 
 	// gets correct answers
@@ -86,37 +69,81 @@ $('#submitButton').click(function(event) {
 		userScore++
 	}
 
-	$('.userScoreTally').text(userScore);
-
-
 	//increment currentQuestion
 
 	currentQuestion++;
 
+	
+
+	//shows answers at end
 	if (currentQuestion >= listOfQuestions.length) {
 		console.log('current answers');
 		for(var i = 0; i < listOfQuestions.length; i++) {
-			var CQ = listOfQuestions[i]
-		var clonedQuestion = $('#clone').clone();
-		clonedQuestion.find('h4').text(CQ.question);
-		(function() {
-			for(var i = 0; i < CQ.options.length; i++) {
-				var option = CQ.options[i]
-				clonedQuestion.find('ol').append('<li>' + option +'</li>')
-			}
-		})();
-		$('.content').hide();
 
-		clonedQuestion.show();
-		$('#correctAnswers > ul').append(clonedQuestion);
+			var CQ = listOfQuestions[i]
+			var clonedQuestion = $('#clone').clone();
+			clonedQuestion.attr("id","clone-" + i);	
+			clonedQuestion.find('h4').text(CQ.question);
+			// (function() {
+			// 	for(var i = 0; i < CQ.options.length; i++) {
+			// 		var option = CQ.options[i]
+					
+
+					var rightIndex = listOfQuestions[i].correctAnswer
+					var rightAnswer = listOfQuestions[i].options[rightIndex]
+					clonedQuestion.find('ol').append('<li>' + rightAnswer +'</li>')			
+
+			//})();
+			
+			$('#correctAnswers > ul').append(clonedQuestion);
 		}
+		$('.content').hide();
+		$('#correctAnswers').show();
+	}
+	
+	//displays question + answers
+	else {
+		updateDisplay();
 	}
 
 
 
-
-
-
-
-
 });
+
+//reset button
+$('.reset').click(function() {
+	currentQuestion = 0;
+	userScore = 0;
+
+	updateDisplay();
+	$('.content').show();
+	$('#correctAnswers').hide();
+	$.each( $("li[id^='clone-']"), function () {
+  		$(this).remove();
+	});
+});
+
+
+
+
+
+
+
+function updateDisplay(){
+	//replace question
+
+	$('#question').text(listOfQuestions[currentQuestion].question);
+
+	//replace list of answers
+
+	for(var i = 0; i < 4; i++ ) {
+
+		$('#choice' + (i + 1) ).text(listOfQuestions[currentQuestion].options[i]);
+	}
+
+	//resets radio button
+
+	$('input[name="userGuess"]').prop('checked', false);
+
+	$('.userScoreTally').text(userScore);
+};
